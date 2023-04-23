f60b188514178dc17d1ed5c68ff070b3acde3c01
bssborka <bakoss.borka@gmail.com>
2023-04-23 19:48:52 +0200

    Initial commit

---
+++ ChatApp/MVVM/Model/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatClient.MVVM.Model
+{
+    class UserModel
+    {
+        public string Username { get; set; }
+        public string UID { get; set; }
+
+    }
+}
